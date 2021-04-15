4de6a29e9c4ae1c7c8f75b96438e2bb017356b42
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-16 01:47:50 +0530

    ADD: handler for onChange event of checkboxes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,10 @@
 const TodoItem = (props) => (
     <div className="todo-item">
-        <input type="checkbox" checked={props.item.completed} />
+        <input
+            type="checkbox"
+            checked={props.item.completed}
+            onChange={() => props.handleChange(props.item.id)}
+        />
         <p>{props.item.text}</p>
     </div>
 );
